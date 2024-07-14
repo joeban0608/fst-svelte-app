725c515e725caa7f5a78345c47f76cf3a1c7bef3
hongbangzhou <joeanddaneil5@gmail.com>
2024-07-14 16:45:51 +0800

    feat: basic event

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,7 @@
 <script>
 	import Countdown from "./components/Countdown.svelte";
+	import OnClickDemo from "./components/OnClickDemo.svelte";
 </script>
 
 <Countdown conutDown={10}></Countdown>
+<OnClickDemo />
---
+++ src/components/OnClickDemo.svelte
@@ -0,0 +1,8 @@
+<script>
+  let name = 'Hello World'
+  const handleClick = (e) =>{
+    console.log('click')
+  }
+</script>
+
+<h1 on:click={handleClick}>{name}</h1>
