106b8ddf40b706ec8dceffd017adc5f8477895c6
hongbangzhou <joeanddaneil5@gmail.com>
2024-07-14 18:02:41 +0800

    feat: bind DOM - bind this example

--- src/components/BindDemo.svelte
+++ src/components/BindDemo.svelte
@@ -12,11 +12,31 @@
 
 <input type="text" bind:value={name}/> -->
 
-<script>
+<!-- <script>
   let selected = 1
   $: console.log('selected', selected)
 </script>
 <input type="radio" bind:group={selected} value={1}/>
 <input type="radio" bind:group={selected} value={2}/>
 <input type="radio" bind:group={selected} value={3}/>
-{selected}
+{selected} -->
+
+<script>
+  import { onMount } from "svelte";
+
+  let canvas;
+  let width;
+  onMount(()=>{
+    console.log('canvas width', canvas.clientWidth)
+    console.log('canvas.getContext', canvas.getContext('2d'))
+    console.log('width', width)
+  })
+</script>
+
+<style>
+  canvas {
+    border: 1px solid red;
+  }
+</style>
+
+<canvas bind:this={canvas} bind:clientWidth={width}/>
