0c5bb15c232adca61fc49120078eff21fc996e82
hongbangzhou <joeanddaneil5@gmail.com>
2024-07-14 16:49:03 +0800

    feat: 使用 modify 語法

--- src/components/OnClickDemo.svelte
+++ src/components/OnClickDemo.svelte
@@ -1,8 +1,15 @@
 <script>
-  let name = 'Hello World'
+  let name = 'Hello World';
+  let once = false;
   const handleClick = (e) =>{
+    // e.preventDefault;
+    // if (!once) {
+    //   once = true
+    //   console.log('click')
+    // }
     console.log('click')
   }
 </script>
 
-<h1 on:click={handleClick}>{name}</h1>
+<!-- modify 用法, once 只能點一次, preventDefault 取消預設點擊事件 -->
+<h1 on:click|once|preventDefault={handleClick}>{name}</h1>
